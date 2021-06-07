bb12ffeafee9c0224e0794f5922a34b72c1e69f6
adamessam528 <85436299+adamessam528@users.noreply.github.com>
2021-06-07 22:15:07 +0200

    added comments to paint class

--- src/application/Paint.java
+++ src/application/Paint.java
@@ -9,15 +9,18 @@ import javafx.stage.Stage;
 
 
 public class Paint extends Application{
+	//loads the main fxml borderpane
 	@FXML
 	private BorderPane mainPane;
 
 	@Override
     public void start(Stage primaryStage) throws Exception {
        try {
+	    //creates new loader and loads the file in the giving location
     	    FXMLLoader load = new FXMLLoader();
     	    load.setLocation(getClass().getResource("/ui/Home.fxml"));
             mainPane=load.load();
+	    //sets the scene and shows it
             Scene scene = new Scene(mainPane);
             primaryStage.setScene(scene);
             primaryStage.show();
